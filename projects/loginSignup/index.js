1f292021182c2edd2b1214169c16605d3ee6dbcb
Brandon Vergara <mont.black.123@gmail.com>
2023-04-12 15:56:42 -0500

    NODEJS y EXPRESSJS

---
+++ projects/loginSignup/index.js
@@ -0,0 +1,72 @@
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/home", function (req, res) {
+
+    res.sendFile(__dirname + "/index.html");
+})
+
+var totalData = [];
+
+app.post("/newRecord", function (req, res) {
+    let userName = req.body.userName;
+    let email = req.body.email;
+    let pass = req.body.pass;
+    let pass2 = req.body.pass2;
+
+
+    let userData = {
+        "name": userName,
+        "email": email,
+        "password": pass
+    }
+
+
+    //totalData = [...totalData, userData];
+    totalData.push(userData);
+
+    console.log("datos recibidos exitosamente");
+    /*  console.log(userData); */
+    console.log(totalData);
+    res.send("información recibida exitosamente")
+});
+
+app.post("/consult", function (req, res) {
+
+    let emailExists = req.body.emailExists;
+    let passExists = req.body.passExists;
+
+    let userDataExists = {
+        "email": emailExists,
+        "password": passExists
+    }
+
+    let userExists, array1, array2;
+
+    //console.log(totalData);
+    for (let i = 0; i <= totalData.length; i++) {
+        console.log("array2 " + i + " " + array2);
+        array1 = userDataExists.email;
+        array2 = totalData[i].email;
+
+
+        if ((array1) === (array2)) {
+            userExists = "existe";
+            break;
+        }
+        else {
+            userExists = "no existe"
+        }
+    }
+    res.send(userExists)
+
+})
+app.listen(3000, function () {
+    console.log("Servidor listo y preparado en el puerto 3000")
+});
+
---
+++ sessions/session_39_nodejs/index.js
@@ -0,0 +1,92 @@
+//servidor creado con NodeJS puro
+
+/* const http = require('http');
+const fs = require('fs');
+
+// creamos un servidor HTTP que maneje las peticiones (request) del cliente 
+// y devuelva una respuesta ( response)
+http.createServer(function (req, res) {
+
+    res.writeHead(200, {
+        "Content-Type": "text/html",
+        "Access-Control-Allow-Origin": "*",
+    });
+
+    // acceder al archivo que quiero enviarle al cliente
+
+    let leerArchivo = fs.createReadStream(__dirname + "/index.html");
+
+    leerArchivo.pipe(res);
+
+}).listen(3000, function () {
+    console.log("servidor escuchando en el puerto 3000");
+}); */
+
+
+// Servidor creado con EXPRESS
+const express = require("express");
+
+const app = express();
+const bodyParser = require("body-parser");
+
+// configurar el bodyparser
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// creamos la URL + METODO = Petición del cliente
+
+//const path = require("path");
+
+
+app.get("/home", function (req, res) { //app.get("/ejemplo", function (req, res)
+
+    console.log("El cliente ingreso por GET a http://localhost:3000/home")
+
+    //respuesta del servidor
+    //res.send("Gracias por comunicarse con nosotros")
+
+    res.sendFile(__dirname + "/index.html")
+});
+// Ruta para GET registro
+app.get("/registro", function (req, res) {
+
+    res.sendFile(__dirname + "/registro.html");
+
+})
+// Ruta para GET login
+
+app.get("/login", function (req, res) {
+
+    res.sendFile(__dirname + "/login.html");
+
+})
+app.post("/formulario", function (req, res) {
+    // caputramos datos de formulario y hacemos algo con ellos
+    console.log("Recibimos la información");
+    console.log(req.body)
+    console.log("email: " + req.body.email)
+    console.log("contraseña: " + req.body.password)
+
+})
+app.get("/calculadora", function (req, res) {
+
+    res.sendFile(__dirname + "/calculadora.html")
+})
+
+app.post("/funcionamiento", function (req, res) {
+    console.log(req.body)
+    let num1 = parseInt(req.body.num1);
+    let num2 = parseInt(req.body.num2);
+    let operation = req.body.operation;
+    let result = 0;
+
+    (operation === "suma") ? result = num1 + num2 : "";
+    (operation === "resta") ? result = num1 - num2 : "";
+    (operation === "multiplicacion") ? result = num1 * num2 : "";
+    (operation === "division") ? ((num2 != 0) ? result = num1 / num2 : console.log("No se puede dividir entre 0")) : "";
+
+    console.log(result)
+})
+app.listen(3000, function () {
+    console.log("Servidor listo y preparado en el puerto 3000")
+});
